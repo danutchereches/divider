1e408f4bbd354cf86276b39fda5276c175a98897
Danut Chereches <load.runner@weednet.ro>
2015-07-21 21:53:02 +0300

    initial import/cloned game-model

---
+++ Classes/GameScene.h
@@ -0,0 +1,46 @@
+#ifndef __GAMESCENE_H__
+#define __GAMESCENE_H__
+
+#include "cocos2d.h"
+#include "Helpers.h"
+#include "Loader.h"
+#include "Objects.h"
+#include "AppDelegate.h"
+
+class GameScene : public cocos2d::LayerColor
+{
+public:
+	// there's no 'id' in cpp, so we recommend returning the class instance pointer
+	static cocos2d::Scene* createScene();
+	
+	// Here's a difference. Method 'init' in cocos2d-x returns bool, instead of returning 'id' in cocos2d-iphone
+	virtual bool init() override;
+	
+	// implement the "static create()" method manually
+	CREATE_FUNC(GameScene);
+
+private:
+	cocos2d::Size mScreenSize;
+	cocos2d::Size mVisibleSize;
+	cocos2d::Vec2 mOrigin;
+	
+	cocos2d::Layer* mGameLayer;
+	cocos2d::Layer* mUILayer;
+	
+	bool mIsGameServicesAvailable;
+	
+	void initPools();
+	
+	void update(float dt);
+	void updateSlow(float dt);
+	
+	bool onTouchBegan(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchMoved(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onTouchEnded(cocos2d::Touch* touch, cocos2d::Event* event);
+	void onKeyPressed(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+	void onKeyReleased(cocos2d::EventKeyboard::KeyCode keyCode, cocos2d::Event* event);
+	void onComeToForeground();
+	void onComeToBackground();
+};
+
+#endif // __GAMESCENE_H__
